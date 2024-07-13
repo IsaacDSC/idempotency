a9a8fc1284e9a208c74ff4d3ef0ade08c5ccec23
IsaacDSC <isaac8.silva@hotmail.com>
2024-07-13 15:54:18 -0300

    feat: add logger

--- idempotency.go
+++ idempotency.go
@@ -3,8 +3,9 @@ package idempotency
 import (
 	"context"
 	"errors"
-	"fmt"
 	"github.com/redis/go-redis/v9"
+	"log/slog"
+	"os"
 	"time"
 )
 
@@ -12,6 +13,7 @@ type IdempotencyKey struct {
 	separator       string
 	revalidateCache bool
 	redisClient     *redis.Conn
+	logger          *slog.Logger
 }
 
 func NewIdempotencyKey(conn *redis.Conn, revalidateCache bool, separator string) *IdempotencyKey {
@@ -19,10 +21,13 @@ func NewIdempotencyKey(conn *redis.Conn, revalidateCache bool, separator string)
 		separator = "-"
 	}
 
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	return &IdempotencyKey{
 		separator:       separator,
 		redisClient:     conn,
 		revalidateCache: revalidateCache,
+		logger:          logger,
 	}
 }
 
@@ -39,7 +44,7 @@ func (ik IdempotencyKey) CreateIdempotencyKey(ctx context.Context, TTL time.Dura
 	}
 
 	if err := ik.redisClient.Set(ctx, key, time.Now().String(), TTL).Err(); err != nil {
-		fmt.Println("logger error set", err)
+		ik.logger.Error("createIdempotencyKey error", "error", err)
 		return "", false
 	}
 
@@ -52,13 +57,13 @@ func (ik IdempotencyKey) alreadyExecuted(ctx context.Context, key string, TTL ti
 	if errors.Is(err, redis.Nil) {
 		return false
 	} else if err != nil {
-		fmt.Println("logger error get", err)
+		ik.logger.Error("alreadyExecuted error", "error", err)
 		return false
 	}
 
 	if ik.revalidateCache {
 		if err := ik.redisClient.Set(ctx, key, v, TTL).Err(); err != nil {
-			fmt.Println("logger error revalidate ttl", err)
+			ik.logger.Error("error revalidate ttl", "error", err)
 		}
 	}
 
@@ -69,7 +74,7 @@ func (ik IdempotencyKey) Commit(ctx context.Context, keys []string) {
 	for i := range keys {
 		if keys[i] != "" {
 			if err := ik.redisClient.Del(ctx, keys[i]).Err(); err != nil {
-				fmt.Println("logger error del", err)
+				ik.logger.Error("", "commit error", err)
 			}
 		}
 	}
