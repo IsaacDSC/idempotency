fd130672c46c6036da598394b472ec09536d49d3
IsaacDSC <isaac8.silva@hotmail.com>
2024-08-04 09:46:58 -0300

    feat: simplify example

--- example/main.go
+++ example/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/IsaacDSC/idempotency"
 	"github.com/redis/go-redis/v9"
@@ -22,43 +23,50 @@ func main() {
 	defer rdb.Close()
 
 	ik := idempotency.NewIdempotencyKey(rdb.Conn(), false, ":")
-
 	orderID := "71230ada-b9fd-480b-836a-0aaecf9944fb"
-	key1, alreadyExec1 := ik.CreateIdempotencyKey(ctx, time.Minute*5, "create-card", orderID)
 
 	var (
 		cardID string
 		err    error
 	)
-	if !alreadyExec1 {
+
+	createCard := func(ctx context.Context) error {
 		//processando k1
+		fmt.Println("processando k1")
 		cardID, err = CreateCard(orderID)
 		if err != nil {
-			log.Fatal(err)
-			return
+			return err
 		}
+
+		return nil
 	}
 
-	key2, alreadyExec2 := ik.CreateIdempotencyKey(ctx, time.Minute*5, "update-card", orderID, cardID)
-	if !alreadyExec2 {
-		//processando k2
+	err = ik.Once(ctx, createCard, time.Minute*5, "create-card", orderID)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	err = ik.Once(ctx, func(ctx context.Context) error {
+		//	//processando k2
 		if err := UpdateCard(cardID, "123"); err != nil {
-			log.Fatal(err)
-			return
+			return err
 		}
-	}
 
-	fmt.Println("Keys", key1, key2)
-	ik.Commit(ctx, []string{key1, key2})
+		return nil
+	}, time.Minute*5, "update-card", orderID, cardID)
+
 }
 
 func CreateCard(orderID string) (cardID string, err error) {
+	fmt.Println("running create card")
 	time.Sleep(time.Second)
 	cardID = "f9097b9b-1cc2-442a-a06a-7aea467dcae2"
 	return
 }
 
 func UpdateCard(cardID string, value string) error {
+	fmt.Println("running update card")
 	time.Sleep(time.Second)
-	return nil
+	return errors.New("updated with error")
 }
